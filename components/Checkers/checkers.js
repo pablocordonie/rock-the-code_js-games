123ac071e412c8b950444bdf22a344909b5a0d50
pablocordonie <pablogarcia.93.06@gmail.com>
2024-03-01 03:14:28 +0100

    feat: begin to develop the first behaviours of the checkers' movements in checkers.js and checkers.css

--- components/Checkers/checkers.js
+++ components/Checkers/checkers.js
@@ -1,6 +1,7 @@
 import './checkers.css'
 import mainMenuCleaner from '../../utils/mainMenuCleaner';
-import { delay } from '../../utils/handleCardClick';
+
+let selectedBoxes = [];
 
 const generateBoard = () => {
     let rowCount = 1;
@@ -19,9 +20,9 @@ const generateBoard = () => {
             checkersBox.className = 'rtc-checkers--board-box';
             checkersBox.classList.add(`box-${boxCount++}`);
             if (j % 2 === 0) {
-                checkersBox.classList.add('color1-box');
-            } else {
                 checkersBox.classList.add('color2-box');
+            } else {
+                checkersBox.classList.add('color1-box');
             }
             checkersRow_1.appendChild(checkersBox);
         }
@@ -37,9 +38,9 @@ const generateBoard = () => {
             checkersBox.className = 'rtc-checkers--board-box';
             checkersBox.classList.add(`box-${boxCount++}`);
             if (k % 2 === 0) {
-                checkersBox.classList.add('color2-box');
-            } else {
                 checkersBox.classList.add('color1-box');
+            } else {
+                checkersBox.classList.add('color2-box');
             }
             checkersRow_2.appendChild(checkersBox);
         }
@@ -48,53 +49,133 @@ const generateBoard = () => {
     return checkersBoard;
 };
 
-const placeChecker = (type, position, count) => {
-    let id = '';
+const getBox = (position) => {
+    const boxSelector = `.row-${position[0]} > .box-${position[1]}`;
+    const box = document.querySelector(boxSelector);
+    return box;
+};
 
+const toCleanSelectedBoxes = () => {
+    selectedBoxes.forEach(selectedBox => selectedBox.classList.remove('selected'));
+    selectedBoxes = [];
+};
+
+const toMoveChecker = (checker, move) => {
+    const actualBox = getBox(checker.position);
+    const newBox = getBox(move);
+
+    actualBox.removeChild(checker.piece);
+    newBox.appendChild(checker.piece);
+    checker.position = move;
+};
+
+const placeChecker = (type, position, count, checkers) => {
+
+    // Creación de cada ficha
     const piece = document.createElement('div');
     piece.className = 'rtc-checkers--board-checker';
-    piece.id = id + '-' + count;
 
     switch (type) {
         case 'dark':
             piece.classList.add('dark_checker');
-            id = 'dark';
+            piece.id = 'dark' + '-' + count;
             break;
         case 'light':
             piece.classList.add('light_checker');
-            id = 'light';
+            piece.id = 'light' + '-' + count;
             break;
         case 'dark-queen':
             piece.classList.add('dark_checker');
             piece.classList.add('queen_checker');
-            id = 'dark-queen';
+            piece.id = 'dark-queen';
             break;
         case 'light-queen':
             piece.classList.add('light_checker');
             piece.classList.add('queen_checker');
-            id = 'light-queen';
+            piece.id = 'light-queen';
             break;
     }
 
-    piece.addEventListener('click', () => console.log('selected piece: ' + piece.id));
+    // Colocar pieza en el DOM
+    getBox(position).appendChild(piece);
 
-    const boxSelector = `.row-${position[0]} > .box-${position[1]}`;
-    const box = document.querySelector(boxSelector);
-    box.appendChild(piece);
+    // Modelo de cada ficha
+    const checker = {
+        id: '',
+        position: '' + position[0] + position[1],
+        type: type,
+        possibleMoves: () => {
+            // Si es oscura, se mueve 1 fila más
+            // Si es clara, se mueve una fila menos
+            // Ambos se pueden mover una columna más ó menos
+            let actualRow = position[0];
+            let actualColumn = position[1];
+
+            let newRow = actualRow + 1;
+            if (type === 'light') {
+                newRow = actualRow - 1;
+            }
+
+            return ['' + newRow + (actualColumn + 1), '' + newRow + (actualColumn - 1)];
+        },
+        active: true,
+        isQueen: false,
+        selected: false,
+        piece
+    };
+
+    // Agregar ficha a la colección
+    checker.id = piece.id;
+    checkers[piece.id] = checker;
+
+    // Agregar evento de click
+    piece.addEventListener('click', () => {
+        const possibleMoves = checker.possibleMoves();
+        toCleanSelectedBoxes();
+
+        possibleMoves.forEach(possibleMove => {
+            const selectedBox = getBox(possibleMove);
+            selectedBox.classList.toggle('selected');
+            selectedBoxes.push(selectedBox);
+            selectedBox.addEventListener('click', () => {
+                toCleanSelectedBoxes();
+                toMoveChecker(checker, possibleMove);
+            });
+        });
+    });
 };
 
-const placeInitialCheckers = (checker) => {
+const placeInitialCheckers = (checkerColor) => {
+    // Devolver un objeto con las 12 fichas
+    const checkers = {};
     let count = 1;
-    const initialRowIndex = checker === 'dark' ? 1 : 6;
+    const initialRowIndex = checkerColor === 'dark' ? 1 : 6;
     const finalRowIndex = initialRowIndex + 2;
 
     for (let i = initialRowIndex; i <= finalRowIndex; i++) {
         for (let j = 1; j <= 4; j++) {
             let subtract = i % 2 === 1 ? 0 : 1;
-            placeChecker(checker, [i, j * 2 - subtract], count);
+            placeChecker(checkerColor, [i, j * 2 - subtract], count, checkers);
             count++;
         }
     }
+
+    return checkers;
+};
+
+const availableBoxesToMove = () => {
+    const boardLimits = 8;
+    const boxes = [];
+
+    for (let rowIndex = 1; rowIndex <= boardLimits; rowIndex++) {
+        for (let columnIndex = 1; columnIndex <= boardLimits; columnIndex++) {
+            if (rowIndex % 2 === 1 && columnIndex % 2 === 0 || columnIndex % 2 === 1 && rowIndex % 2 === 0) {
+                boxes.push('' + rowIndex + columnIndex);
+            }
+        }
+    }
+
+    return boxes;
 };
 
 const checkersTemplate = (event) => {
@@ -126,6 +207,7 @@ const checkersTemplate = (event) => {
         checkersMain.appendChild(checkersGame);
         placeInitialCheckers('dark');
         placeInitialCheckers('light');
+        availableBoxesToMove();
     }
 };
 
